ba399e3efbb5dd902849d48c7a3a413e2a532190
Jay <jay@thefullerlife.com>
2024-09-03 15:12:34 +0530

    ENV variable setups

--- app/api/route.js
+++ app/api/route.js
@@ -3,3 +3,6 @@ import { NextResponse } from "next/server";
 export async function GET(req) {
     return NextResponse.json({time: new Date().toLocaleString() });
 }
+
+
+// mongodb+srv://jay:dXyhnOFRY7lVdrw4@cluster0.wmpn4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
